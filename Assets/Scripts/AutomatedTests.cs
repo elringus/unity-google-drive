753de8007a2bca61bf83d6527349f56f6a6db358
Artyom Sovetnikov <elringus@yandex.ru>
2017-11-15 18:47:18 +0300

    implement automated tests

---
+++ Assets/Scripts/AutomatedTests.cs
@@ -0,0 +1,92 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class AutomatedTests
+{
+    private const string TEST_RESOURCE_PATH = "TestImage";
+
+    private string createdFileId;
+    private string copiedFileId;
+
+    [UnityTest]
+    public IEnumerator Test001_AboutGet ()
+    {
+        var request = GoogleDriveAbout.Get();
+        request.Fields = new List<string> { "user" };
+        yield return request.Send();
+        Assert.IsFalse(request.IsError);
+    }
+
+    [UnityTest]
+    public IEnumerator Test002_FilesList ()
+    {
+        var request = GoogleDriveFiles.List();
+        yield return request.Send();
+        Assert.IsFalse(request.IsError);
+    }
+
+    [UnityTest]
+    public IEnumerator Test003_FilesCreate ()
+    {
+        var content = Resources.Load<Texture2D>(TEST_RESOURCE_PATH).EncodeToPNG();
+        var file = new Data.File() { Name = "AutoTestUpload", Content = content };
+        var request = GoogleDriveFiles.Create(file);
+        request.Fields = new List<string> { "id" };
+        yield return request.Send();
+        Assert.IsFalse(request.IsError);
+        Assert.NotNull(request.ResponseData.Id);
+        createdFileId = request.ResponseData.Id;
+    }
+
+    [UnityTest]
+    public IEnumerator Test004_FilesGet ()
+    {
+        var request = GoogleDriveFiles.Get(createdFileId);
+        yield return request.Send();
+        Assert.IsFalse(request.IsError);
+    }
+
+    [UnityTest]
+    public IEnumerator Test005_FilesDownload ()
+    {
+        var request = GoogleDriveFiles.Download(createdFileId);
+        yield return request.Send();
+        Assert.IsFalse(request.IsError);
+        Assert.NotNull(request.ResponseData.Content);
+        Assert.Greater(request.ResponseData.Content.Length, 0);
+    }
+
+    [UnityTest]
+    public IEnumerator Test006_FilesCopy ()
+    {
+        var file = new Data.File() { Id = createdFileId };
+        var request = GoogleDriveFiles.Copy(file);
+        request.Fields = new List<string> { "id" };
+        yield return request.Send();
+        Assert.IsFalse(request.IsError);
+        Assert.NotNull(request.ResponseData.Id);
+        copiedFileId = request.ResponseData.Id;
+    }
+
+    [UnityTest]
+    public IEnumerator Test007_FilesEmptyTrash ()
+    {
+        var request = GoogleDriveFiles.EmptyTrash();
+        yield return request.Send();
+        Assert.IsFalse(request.IsError);
+    }
+
+    [UnityTest]
+    public IEnumerator Test999_FilesDelete ()
+    {
+        var request1 = GoogleDriveFiles.Delete(createdFileId);
+        yield return request1.Send();
+        Assert.IsFalse(request1.IsError);
+        var request2 = GoogleDriveFiles.Delete(copiedFileId);
+        yield return request2.Send();
+        Assert.IsFalse(request2.IsError);
+    }
+}
