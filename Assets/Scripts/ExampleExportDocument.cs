5a97dfb756d31c2b75c8544c22e3f30f4971f4ea
Artyom Sovetnikov <elringus@yandex.ru>
2017-12-21 20:01:58 +0300

    add example for exporting google docs as text files

---
+++ Assets/Scripts/ExampleExportDocument.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using UnityEngine;
+
+public class ExampleExportDocument : MonoBehaviour
+{
+    public Rect WindowRect = new Rect(10, 10, 300, 200);
+
+    private GoogleDriveFiles.ExportRequest request;
+    private string fileId = string.Empty;
+    private string result = string.Empty;
+
+    private void OnGUI ()
+    {
+        GUILayout.Window(0, WindowRect, InfoWindowGUI, "Export Google Document");
+    }
+
+    private void InfoWindowGUI (int windowId)
+    {
+        if (request != null && request.IsRunning)
+        {
+            GUILayout.Label(string.Format("Loading: {0:P2}", request.Progress));
+        }
+        else
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Doc file ID:", GUILayout.Width(85));
+            fileId = GUILayout.TextField(fileId);
+            if (GUILayout.Button("Download", GUILayout.Width(100))) ExportDocument();
+            GUILayout.EndHorizontal();
+
+            GUILayout.Label("Result:", GUILayout.Width(85));
+            GUILayout.TextArea(result);
+        }
+    }
+
+    private void ExportDocument ()
+    {
+        request = GoogleDriveFiles.Export(fileId, "text/plain");
+        request.Send().OnDone += file => result = Encoding.UTF8.GetString(file.Content);
+    }
+}
