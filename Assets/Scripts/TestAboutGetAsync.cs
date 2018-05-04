81a5aebda79a1d8816afd5220ebc0352e4fae904
Artyom Sovetnikov <elringus@yandex.ru>
2018-05-04 19:46:29 +0300

    add async extensions

---
+++ Assets/Scripts/TestAboutGetAsync.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityGoogleDrive;
+
+public class TestAboutGetAsync : MonoBehaviour
+{
+    #if NET_4_6
+    public Rect WindowRect = new Rect(10, 10, 940, 580);
+
+    private GoogleDriveAbout.GetRequest request;
+    private UnityGoogleDrive.Data.About aboutData;
+         
+    private async void Start () => aboutData = await UpdateInfo();
+
+    private void OnGUI () => GUILayout.Window(0, WindowRect, InfoWindowGUI, "Google Drive Info (Async)");
+
+    private void InfoWindowGUI (int windowId)
+    {
+        if (aboutData != null)
+        {
+            GUILayout.Label(string.Format("User name: {0}\nUser email: {1}\nSpace used: {2:0}/{3:0} MB",
+                aboutData.User.DisplayName,
+                aboutData.User.EmailAddress,
+                aboutData.StorageQuota.Usage * .000001f,
+                aboutData.StorageQuota.Limit * .000001f));
+        }
+        else if (request != null && request.IsRunning) GUILayout.Label(string.Format("Loading: {0:P2}", request.Progress));
+    }
+
+    private async System.Threading.Tasks.Task<UnityGoogleDrive.Data.About> UpdateInfo ()
+    {
+        request = GoogleDriveAbout.Get();
+        request.Fields = new List<string> { "user", "storageQuota" };
+        return await request.Send();
+    }
+
+    #else
+    private void OnGUI () { GUILayout.Label(".NET 4.x scripting backend is not enabled."); }
+    #endif
+}
---
+++ Assets/UnityGoogleDrive/Runtime/Utilities/AsyncExtensions.cs
@@ -0,0 +1,22 @@
+﻿#if NET_4_6
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace UnityGoogleDrive
+{
+    public static class AsyncExtensions
+    {
+        /// <summary>
+        /// Allows awaiting <see cref="GoogleDriveRequest"/> objects in async methods.
+        /// </summary>
+        public static TaskAwaiter<TResponse> GetAwaiter<TResponse> (this GoogleDriveRequestYeildInstruction<TResponse> yeildInstruction)
+        {
+            var taskCompletionSource = new TaskCompletionSource<TResponse>();
+            if (yeildInstruction.GoogleDriveRequest.IsDone) taskCompletionSource.SetResult(yeildInstruction.GoogleDriveRequest.ResponseData);
+            else yeildInstruction.OnDone += responseData => taskCompletionSource.SetResult(responseData);
+            return taskCompletionSource.Task.GetAwaiter();
+        }
+
+    }
+}
+#endif
