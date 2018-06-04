04476166960d15a490b9ab93d04cdaa0cbe2ad1d
Artyom Sovetnikov <elringus@yandex.ru>
2018-06-04 23:47:04 +0300

    add helpers

---
+++ Assets/Scripts/ExampleFindFilesByPathAsync.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityGoogleDrive;
+
+public class ExampleFindFilesByPathAsync : MonoBehaviour
+{
+    #if NET_4_6 || NET_STANDARD_2_0
+    public Rect WindowRect = new Rect(10, 10, 940, 580);
+
+    private bool running;
+    private string filePath = string.Empty;
+    private bool folder;
+    private Dictionary<string, string> results;
+    private Vector2 scrollPos;
+
+    private void OnGUI ()
+    {
+        GUILayout.Window(0, WindowRect, InfoWindowGUI, "Find Files By Path");
+    }
+
+    private void InfoWindowGUI (int windowId)
+    {
+        if (running) GUILayout.Label("Loading, please wait...");
+
+        if (!running && results != null)
+        {
+            scrollPos = GUILayout.BeginScrollView(scrollPos);
+            foreach (var result in results)
+            {
+                GUILayout.Label(result.Value);
+                if (string.IsNullOrEmpty(result.Key)) continue;
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("ID:", GUILayout.Width(20));
+                GUILayout.TextField(result.Key);
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.EndScrollView();
+        }
+
+        if (!running)
+        {
+            GUILayout.BeginHorizontal();
+            folder = GUILayout.Toggle(folder, "Folder", GUILayout.Width(65));
+            GUILayout.Label("File path:", GUILayout.Width(70));
+            filePath = GUILayout.TextField(filePath);
+            if (GUILayout.Button("Search", GUILayout.Width(100))) FindFilesByPathAsync(filePath);
+            GUILayout.EndHorizontal();
+        }
+    }
+
+    private async void FindFilesByPathAsync (string path)
+    {
+        running = true;
+        var files = await Helpers.FindFilesByPathAsync(path, fields: new List<string> { "files(id, name, size, modifiedTime)" }, mime: folder ? Helpers.FOLDER_MIME_TYPE : null);
+        BuildResults(files);
+        running = false;
+    }
+
+    private void BuildResults (List<UnityGoogleDrive.Data.File> fileList)
+    {
+        if (fileList.Count == 0) { results = new Dictionary<string, string> { [string.Empty] = "Non files found." }; return; }
+
+        results = new Dictionary<string, string>();
+
+        foreach (var file in fileList)
+        {
+            var fileInfo = string.Format("Name: {0} Size: {1:0.00}MB Modified: {2:dd.MM.yyyy}",
+                file.Name,
+                file.Size * .000001f,
+                file.ModifiedTime);
+            results.Add(file.Id, fileInfo);
+        }
+    }
+
+    #else
+    private void OnGUI () { GUILayout.Label(".NET 4.x scripting backend is not enabled."); }
+    #endif
+}
---
+++ Assets/UnityGoogleDrive/Runtime/Utilities/Helpers.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace UnityGoogleDrive
+{
+    public static class Helpers
+    {
+        /// <summary>
+        /// The MIME type of a <see cref="Data.File"/> representing a folder in Google Drive.
+        /// </summary>
+        /// <remarks>
+        /// A folder in Google Drive is actually a file with the special MIME type. 
+        /// More info: https://developers.google.com/drive/api/v3/folder.
+        /// </remarks>
+        public const string FOLDER_MIME_TYPE = "application/vnd.google-apps.folder";
+        /// <summary>
+        /// ID alias of the drive's root folder.
+        /// </summary>
+        public const string ROOT_ALIAS = "root";
+        /// <summary>
+        /// ID alias of the app data folder.
+        /// </summary>
+        public const string APPDATA_ALIAS = "appDataFolder";
+        /// <summary>
+        /// Main user's space, starting at the root.
+        /// </summary>
+        public const string DRIVE_SPACE = "drive";
+        /// <summary>
+        /// A special hidden space that your app can use to store application data.
+        /// </summary>
+        public const string APPDATA_SPACE = "appDataFolder";
+
+        #if NET_4_6 || NET_STANDARD_2_0
+        /// <summary>
+        /// Looks for the files located at the provided path.
+        /// </summary>
+        /// <param name="path">File's path in Google Drive. Add front slash to find all files in a folder.</param>
+        /// <param name="appData">Whether to look inside the AppData folder instead of the drive root.</param>
+        /// <param name="fields">File fields to request.</param>
+        /// <param name="mimeType">File's MIME type.</param>
+        /// <param name="trashed">Whether to include trashed files.</param>
+        public static async System.Threading.Tasks.Task<List<Data.File>> FindFilesByPathAsync (string path, bool appData = false, List<string> fields = null, string mime = null, bool trashed = false)
+        {
+            var result = new List<Data.File>();
+
+            var fileName = Path.GetFileName(path);
+
+            var parentIds = await ValidatePath(path, appData);
+            if (parentIds == null) return result;
+
+            var parentQueries = parentIds.Select(parentId => $"'{parentId}' in parents");
+            var query = $"({string.Join(" or ", parentQueries)}) and trashed = {trashed}";
+            if (!string.IsNullOrWhiteSpace(fileName)) query += $" and name = '{fileName}' ";
+            if (!string.IsNullOrWhiteSpace(mime)) query += $" and mimeType = '{mime}'";
+
+            if (fields == null) fields = new List<string>();
+            fields.Add("nextPageToken, files(id)");
+
+            string pageToken = null;
+            do
+            {
+                var listRequest = GoogleDriveFiles.List(query, fields, appData ? APPDATA_SPACE : DRIVE_SPACE, pageToken);
+                var fileList = await listRequest.Send();
+                if (fileList?.Files?.Count > 0) result.AddRange(fileList.Files);
+                pageToken = fileList?.NextPageToken;
+                listRequest.Dispose();
+
+            } while (pageToken != null);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Traverses the entire path and returns ID of the top-level folder(s) (if found), null otherwise.
+        /// </summary>
+        /// <param name="path">Path to validate.</param>
+        /// <returns>ID of the final folders in the path; null if the path is not valid.</returns>
+        public static async System.Threading.Tasks.Task<HashSet<string>> ValidatePath (string path, bool appData, string parentId = null)
+        {
+            if (parentId == null) parentId = appData ? APPDATA_ALIAS : ROOT_ALIAS;
+            if (string.IsNullOrWhiteSpace(path)) return new HashSet<string> { parentId };
+            path = Path.GetDirectoryName(path).Replace('\\', '/');
+            if (string.IsNullOrWhiteSpace(path) || path.Trim() == "/" || path.Trim() == "\\") return new HashSet<string> { parentId };
+            var parentNames = path.Split('/').Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+            var result = new HashSet<string>();
+
+            for (int i = 0; i < parentNames.Length; i++)
+            {
+                using (var listRequest = new GoogleDriveFiles.ListRequest())
+                {
+                    listRequest.Fields = new List<string> { "files(id, name)" };
+                    listRequest.Spaces = appData ? APPDATA_SPACE : DRIVE_SPACE;
+                    listRequest.PageSize = 1000; // Assume we can't have more than 1K folders with equal names at the same level and skip the pagination.
+                    listRequest.Q = $"'{parentId}' in parents and name = '{parentNames[i]}' and mimeType = '{FOLDER_MIME_TYPE}' and trashed = false";
+
+                    await listRequest.Send();
+
+                    if (listRequest == null || listRequest.IsError || listRequest.ResponseData.Files == null || listRequest.ResponseData.Files.Count == 0)
+                        return null;
+
+                    // When at the top level, add all the folder's IDs.
+                    if (i + 1 == parentNames.Length) { result.UnionWith(listRequest.ResponseData.Files.Select(f => f.Id)); break; }
+
+                    // Multiple folders with equal names found at the same level (but not top), travers them recursively.
+                    if (listRequest.ResponseData.Files.Count > 1)
+                    {
+                        var nextPath = string.Join("/", parentNames.Skip(i + 1)) + "/";
+                        foreach (var folder in listRequest.ResponseData.Files)
+                        {
+                            var topFolderIds = await ValidatePath(nextPath, appData, folder.Id);
+                            if (topFolderIds != null) result.UnionWith(topFolderIds);
+                        }
+                        break;
+                    } // Only one folder found, use it's ID to travers higher.
+                    else parentId = listRequest.ResponseData.Files[0].Id;
+                }
+            }
+
+            return result.Count == 0 ? null : result;
+        }
+        #endif
+    }
+}
