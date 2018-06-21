1ae96384da542b90e42a8d708e3be8f127bef8b3
Artyom Sovetnikov <elringus@yandex.ru>
2018-06-21 22:22:26 +0300

    update third party

--- Assets/ThirdParty/UnityCommon/Editor/PackageExporter.cs
+++ Assets/ThirdParty/UnityCommon/Editor/PackageExporter.cs
@@ -1,15 +1,15 @@
 ﻿// Copyright 2012-2018 Elringus (Artyom Sovetnikov). All Rights Reserved.
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
 namespace UnityCommon
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Text;
-    using UnityEditor;
-    using UnityEngine;
-    
     [InitializeOnLoad]
     public class PackageExporter : EditorWindow
     {
@@ -18,45 +18,36 @@ namespace UnityCommon
         protected static string AssetsPath { get { return "Assets/" + PackageName; } }
         protected static string OutputPath { get { return PlayerPrefs.GetString(PREFS_PREFIX + "OutputPath"); } set { PlayerPrefs.SetString(PREFS_PREFIX + "OutputPath", value); } }
         protected static string OutputFileName { get { return PackageName; } }
-        protected static string NamespaceToWrap { get { return PlayerPrefs.GetString(PREFS_PREFIX + "NamespaceToWrap"); } set { PlayerPrefs.SetString(PREFS_PREFIX + "NamespaceToWrap", value); } }
         protected static string IgnoredPaths { get { return PlayerPrefs.GetString(PREFS_PREFIX + "IgnoredPaths"); } set { PlayerPrefs.SetString(PREFS_PREFIX + "IgnoredPaths", value); } }
         private static bool IsAnyPathsIgnored { get { return !string.IsNullOrEmpty(IgnoredPaths); } }
         protected static bool IsReadyToExport { get { return !string.IsNullOrEmpty(OutputPath) && !string.IsNullOrEmpty(OutputFileName); } }
-    
-        private const string TEMP_FOLDER_PATH = "!TEMP_PACKAGE_EXPORTER";
-        private const string SKIP_WRAP_TERM = "PackageExporter: SkipWrap";
+
+        private const string TEMP_FOLDER_NAME = "!TEMP_PACKAGE_EXPORTER";
         private const string PREFS_PREFIX = "PackageExporter.";
         private const string TAB_CHARS = "    ";
-    
+
         private static Dictionary<string, string> modifiedScripts = new Dictionary<string, string>();
-    
+
         private void Awake ()
         {
             if (string.IsNullOrEmpty(PackageName))
                 PackageName = Application.productName;
         }
-    
+
         [MenuItem("Edit/Project Settings/Package Exporter")]
         private static void OpenSettingsWindow ()
         {
             var window = GetWindow<PackageExporter>();
             window.Show();
         }
-    
+
         [MenuItem("Assets/+ Export Package", priority = 20)]
         private static void ExportPackage ()
         {
             if (IsReadyToExport)
-                Export();
+                ExportPackageImpl();
         }
-    
-        [MenuItem("Assets/+ Export Package (Wrap)", priority = 20)]
-        private static void ExportPackageWrapped ()
-        {
-            if (IsReadyToExport)
-                Export(true);
-        }
-    
+
         private void OnGUI ()
         {
             EditorGUILayout.LabelField("Package Exporter Settings", EditorStyles.boldLabel);
@@ -64,7 +55,6 @@ namespace UnityCommon
             EditorGUILayout.Space();
             PackageName = EditorGUILayout.TextField("Package Name", PackageName);
             Copyright = EditorGUILayout.TextField("Copyright Notice", Copyright);
-            NamespaceToWrap = EditorGUILayout.TextField("Namespace", NamespaceToWrap);
             using (new EditorGUILayout.HorizontalScope())
             {
                 OutputPath = EditorGUILayout.TextField("Output Path", OutputPath);
@@ -75,22 +65,22 @@ namespace UnityCommon
             EditorGUILayout.LabelField("Ignored paths (split with new line, start with 'Assets/...'): ");
             IgnoredPaths = EditorGUILayout.TextArea(IgnoredPaths);
         }
-    
-        private static void Export (bool wrapNamespace = false)
+
+        private static void ExportPackageImpl ()
         {
             // Temporary move-out ignored assets.
             DisplayProgressBar("Moving-out ignored assets...", 0f);
             var tmpFolderPath = string.Empty;
             if (IsAnyPathsIgnored)
             {
-                var tmpFolderGuid = AssetDatabase.CreateFolder("Assets", TEMP_FOLDER_PATH);
+                var tmpFolderGuid = AssetDatabase.CreateFolder("Assets", TEMP_FOLDER_NAME);
                 tmpFolderPath = AssetDatabase.GUIDToAssetPath(tmpFolderGuid);
                 var ignoredPaths = IgnoredPaths.SplitByNewLine().ToList();
                 foreach (var path in AssetDatabase.GetAllAssetPaths())
                 {
                     if (!path.StartsWith(AssetsPath)) continue;
                     if (!ignoredPaths.Exists(p => path.StartsWith(p))) continue;
-    
+
                     var movePath = path.Replace(AssetsPath, tmpFolderPath);
                     var moveDirectory = movePath.GetBeforeLast("/");
                     if (!Directory.Exists(moveDirectory))
@@ -98,52 +88,44 @@ namespace UnityCommon
                         Directory.CreateDirectory(moveDirectory);
                         AssetDatabase.Refresh();
                     }
-    
+
                     AssetDatabase.MoveAsset(path, path.Replace(AssetsPath, tmpFolderPath));
                 }
             }
-    
+
             // Modify scripts (namespace and copyright).
             DisplayProgressBar("Modifying scripts...", .25f);
             modifiedScripts.Clear();
-            var needToModify = !string.IsNullOrEmpty(NamespaceToWrap) || !string.IsNullOrEmpty(Copyright);
+            var needToModify = !string.IsNullOrEmpty(Copyright);
             if (needToModify)
             {
                 foreach (var path in AssetDatabase.GetAllAssetPaths())
                 {
                     if (!path.StartsWith(AssetsPath)) continue;
-                    if (!path.EndsWith(".cs")) continue;
-    
+                    if (!path.EndsWith(".cs") && !path.EndsWith(".shader") && !path.EndsWith(".cginc")) continue;
+
                     var fullpath = Application.dataPath.Replace("Assets", "") + path;
                     var originalScriptText = File.ReadAllText(fullpath, Encoding.UTF8);
-    
-                    if (originalScriptText.Contains(SKIP_WRAP_TERM) && !path.EndsWith("PackageExporter.cs")) continue;
-    
+
                     string scriptText = string.Empty;
                     var isImportedScript = path.Contains("ThirdParty");
-    
+
                     var copyright = isImportedScript || string.IsNullOrEmpty(Copyright) ? string.Empty : "// " + Copyright;
-                    if (!string.IsNullOrEmpty(copyright) && (!isImportedScript || wrapNamespace))
+                    if (!string.IsNullOrEmpty(copyright) && !isImportedScript)
                         scriptText += copyright + Environment.NewLine + Environment.NewLine;
-    
-                    if (!string.IsNullOrEmpty(NamespaceToWrap))
-                    {
-                        if (!isImportedScript || wrapNamespace) scriptText += "namespace " + NamespaceToWrap + Environment.NewLine + "{" + Environment.NewLine;
-                        scriptText += isImportedScript ? originalScriptText : TAB_CHARS + originalScriptText.Replace(Environment.NewLine, Environment.NewLine + TAB_CHARS);
-                        if (!isImportedScript || wrapNamespace) scriptText += Environment.NewLine + "}" + Environment.NewLine;
-                    }
-                    else scriptText += originalScriptText;
-    
+
+                    scriptText += originalScriptText;
+
                     File.WriteAllText(fullpath, scriptText, Encoding.UTF8);
-    
+
                     modifiedScripts.Add(fullpath, originalScriptText);
                 }
             }
-    
+
             // Export the package.
             DisplayProgressBar("Writing package file...", .5f);
             AssetDatabase.ExportPackage(AssetsPath, OutputPath + "/" + OutputFileName + ".unitypackage", ExportPackageOptions.Recurse);
-    
+
             // Restore modified scripts.
             DisplayProgressBar("Restoring modified scripts...", .75f);
             if (needToModify)
@@ -151,7 +133,7 @@ namespace UnityCommon
                 foreach (var modifiedScript in modifiedScripts)
                     File.WriteAllText(modifiedScript.Key, modifiedScript.Value, Encoding.UTF8);
             }
-    
+
             // Restore moved-out ignored assets.
             DisplayProgressBar("Restoring moved-out ignored assets...", .95f);
             if (IsAnyPathsIgnored)
@@ -161,17 +143,16 @@ namespace UnityCommon
                     if (!path.StartsWith(tmpFolderPath)) continue;
                     AssetDatabase.MoveAsset(path, path.Replace(tmpFolderPath, AssetsPath));
                 }
-    
+
                 AssetDatabase.DeleteAsset(tmpFolderPath);
             }
-    
+
             EditorUtility.ClearProgressBar();
         }
-    
+
         private static void DisplayProgressBar (string activity, float progress)
         {
             EditorUtility.DisplayProgressBar(string.Format("Exporting {0}", PackageName), activity, progress);
         }
     }
-    
 }
--- Assets/ThirdParty/UnityCommon/Runtime/SceneSwitcher.cs
+++ Assets/ThirdParty/UnityCommon/Runtime/SceneSwitcher.cs
@@ -1,25 +1,23 @@
 ﻿// Copyright 2012-2018 Elringus (Artyom Sovetnikov). All Rights Reserved.
 
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
 namespace UnityCommon
 {
-    using UnityEngine;
-    using UnityEngine.SceneManagement;
-    
-    public class SceneSwitcher : MonoBehaviour 
+    public class SceneSwitcher : MonoBehaviour
     {
         private const int BUTTON_HEIGHT = 50;
         private const int BUTTON_WIDTH = 150;
-    
+
         private void OnGUI ()
-    	{
+        {
             for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
             {
-                var xPos = Screen.width - BUTTON_WIDTH;
                 var yPos = Screen.height - (BUTTON_HEIGHT + (BUTTON_HEIGHT * i));
                 var scene = SceneUtility.GetScenePathByBuildIndex(i).GetAfter("/").GetBefore(".");
-                if (GUI.Button(new Rect(xPos, yPos, BUTTON_WIDTH, BUTTON_HEIGHT), scene)) SceneManager.LoadScene(i);
+                if (GUI.Button(new Rect(0, yPos, BUTTON_WIDTH, BUTTON_HEIGHT), scene)) SceneManager.LoadScene(i);
             }
-    	}
+        }
     }
-    
 }
