b0ebf772575771a2ac98a3cb4f5ed0130ae62aec
Artyom Sovetnikov <elringus@yandex.ru>
2019-07-20 16:19:55 +0300

    update scripts

--- publish.sh
+++ publish.sh
@@ -7,6 +7,8 @@ cd Assets/UnityGoogleDrive
 
 git init
 git add -A
+git reset -- Resources/
+git reset -- Resources.meta
 git commit -m 'publish'
 git push -f git@github.com:Elringus/UnityGoogleDrive.git master:package
 
