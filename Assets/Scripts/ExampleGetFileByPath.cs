e51aa420a3b1022cf6937883b0a5d9b4cea5415b
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-05-19 15:29:14 +0300

    formatting

--- Assets/Scripts/ExampleGetFileByPath.cs
+++ Assets/Scripts/ExampleGetFileByPath.cs
@@ -37,24 +37,24 @@ public class ExampleGetFileByPath : AdaptiveWindowGUI
 
     private IEnumerator GetFileByPathRoutine (string filePath)
     {
-        // A folder in Google Drive is actually a file with the MIME type 'application/vnd.google-apps.folder'. 
-        // Hierarchy relationship is implemented via File's 'Parents' property. To get the actual file using it's path 
-        // we have to find ID of the file's parent folder, and for this we need IDs of all the folders in the chain. 
-        // Thus, we need to traverse the entire hierarchy chain using List requests. 
+        // A folder in Google Drive is actually a file with the MIME type 'application/vnd.google-apps.folder'.
+        // Hierarchy relationship is implemented via File's 'Parents' property. To get the actual file using it's path
+        // we have to find ID of the file's parent folder, and for this we need IDs of all the folders in the chain.
+        // Thus, we need to traverse the entire hierarchy chain using List requests.
         // More info about the Google Drive folders: https://developers.google.com/drive/v3/web/folder.
 
         var fileName = filePath.Contains("/") ? filePath.GetAfter("/") : filePath;
         var parentNames = filePath.Contains("/") ? filePath.GetBeforeLast("/").Split('/') : null;
 
         // Resolving folder IDs one by one to find ID of the file's parent folder.
-        var parendId = "root"; // 'root' is alias ID for the root folder in Google Drive.
+        var parentId = "root"; // 'root' is alias ID for the root folder in Google Drive.
         if (parentNames != null)
         {
             for (int i = 0; i < parentNames.Length; i++)
             {
                 request = new GoogleDriveFiles.ListRequest();
                 request.Fields = new List<string> { "files(id)" };
-                request.Q = $"'{parendId}' in parents and name = '{parentNames[i]}' and mimeType = 'application/vnd.google-apps.folder' and trashed = false";
+                request.Q = $"'{parentId}' in parents and name = '{parentNames[i]}' and mimeType = 'application/vnd.google-apps.folder' and trashed = false";
 
                 yield return request.Send();
 
@@ -67,14 +67,14 @@ public class ExampleGetFileByPath : AdaptiveWindowGUI
                 if (request.ResponseData.Files.Count > 1)
                     Debug.LogWarning($"Multiple '{parentNames[i]}' folders been found.");
 
-                parendId = request.ResponseData.Files[0].Id;
+                parentId = request.ResponseData.Files[0].Id;
             }
         }
 
         // Searching the file.
         request = new GoogleDriveFiles.ListRequest();
         request.Fields = new List<string> { "files(id, size, modifiedTime)" };
-        request.Q = $"'{parendId}' in parents and name = '{fileName}'";
+        request.Q = $"'{parentId}' in parents and name = '{fileName}'";
 
         yield return request.Send();
 
