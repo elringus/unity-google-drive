c02a61b489950121bf9d300edcdb78b34e15d5a7
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-11-16 23:32:46 +0300

    throw error

--- Assets/UnityGoogleDrive/Runtime/Utilities/Helpers.cs
+++ Assets/UnityGoogleDrive/Runtime/Utilities/Helpers.cs
@@ -140,7 +140,7 @@ namespace UnityGoogleDrive
 
                         await listRequest.Send();
 
-                        if (listRequest == null || listRequest.IsError) return null;
+                        if (listRequest.IsError) throw new Error(listRequest.Error);
 
                         // Next folder at the current level is missing; create it.
                         if (listRequest.ResponseData.Files == null || listRequest.ResponseData.Files.Count == 0)
@@ -198,7 +198,8 @@ namespace UnityGoogleDrive
 
                     await listRequest.Send();
 
-                    if (listRequest == null || listRequest.IsError || listRequest.ResponseData.Files == null || listRequest.ResponseData.Files.Count == 0)
+                    if (listRequest.IsError) throw new Error(listRequest.Error);
+                    if (listRequest.ResponseData.Files == null || listRequest.ResponseData.Files.Count == 0)
                         return null;
 
                     // When at the top level, add all the folder's IDs.
